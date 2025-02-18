07db73c52ff325c466e3a0a52c0ab1dca17dc691
grossmannjenna <grossmannjenna@gmail.com>
2025-02-18 14:00:46 -0500

    finished RSA signature forgery

--- bleichenbacher.py
+++ bleichenbacher.py
@@ -20,7 +20,30 @@ def main():
     # TODO: Forge a signature
     #
 
-    forged_signature = 0
+    # get the message hash
+    message_digest = hashlib.sha256(message.encode()).digest()
+
+    # use the ASN.1 bytes
+    asn1_bytes = bytes.fromhex("3031300d060960864801650304020105000420")
+
+    # necessary front padding
+    padding = b'\x00\x01\xff\x00'
+
+    # create 201 arbitrary bytes to add at the end
+    arbitrary_bytes = b'\x00' * 201
+
+    forged_signature_value = padding + asn1_bytes + message_digest + arbitrary_bytes
+
+    # turn the bytes into a big integer
+    forged_int = int.from_bytes(forged_signature_value, byteorder='big')
+
+    # find the cube root
+    forced_sig_cube, is_exact = integer_nthroot(forged_int, 3)
+    # if the cube root is not exact, round to next whole number
+    if not is_exact:
+        forced_sig_cube += 1
+
+    forged_signature = forced_sig_cube
     print(bytes_to_base64(integer_to_bytes(forged_signature, 256)))
 
 
